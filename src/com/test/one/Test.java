9da40ed1c59a21b3c96ab8e41b666089b65809de
0MOoN0 <814429032@qq.com>
2019-08-18 17:40:33 +0800

    新增部分题解

--- src/com/test/one/Test.java
+++ src/com/test/one/Test.java
@@ -1,16 +1,18 @@
 package com.test.one;
 
 public class Test {
-	public static void main(String[] args) {
-		Object o1 = true ? new Integer(1) : new Double(2.0);
-		Object o2;
-		if (true) {
-			o2 = new Integer(1);
-		} else {
-			o2 = new Double(2.0);
-		}
-		System.out.print(o1);
-		System.out.print(" ");
-		System.out.print(o2);
+	
+	public static void a() {
+		
+	}
+	
+	public static void b() {
+	}
+	
+	public void c() {
+		
+	}
+	
+	public static void main(String args[]) {
 	}
 }

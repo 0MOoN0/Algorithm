cd227b23015ae215ded51a9acc4aca6a2ae57da6
0MOoN0 <814429032@qq.com>
2019-08-29 14:51:59 +0800

    新增leetcode-七八三_二叉搜索树节点最小距离

---
+++ src/com/interview/leetcode/七八三_二叉搜索树节点最小距离/Solution.java
@@ -0,0 +1,33 @@
+package com.interview.leetcode.七八三_二叉搜索树节点最小距离;
+
+
+/**
+ * WA
+ * 
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    int ans;
+    
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        dfs(root, ans);
+        return ans;
+    }
+    
+    public void dfs(TreeNode root, int val) {
+    	if(root!=null) {
+    		ans = Math.min(Math.abs(root.val-val),ans);
+    		dfs(root.left, root.val);
+    		dfs(root.right, root.val);
+    	}
+    }
+    
+}
---
+++ src/com/interview/leetcode/七八三_二叉搜索树节点最小距离/solution/Solution.java
@@ -0,0 +1,25 @@
+package com.interview.leetcode.七八三_二叉搜索树节点最小距离.solution;
+
+public class Solution {
+	    TreeNode pre;
+	    int min = Integer.MAX_VALUE;
+	
+	    public int minDiffInBST(TreeNode root) {
+	        inorder(root);
+	        return min;
+	    }
+	
+	    public void inorder(TreeNode node) {
+	        if (node == null)
+	            return;
+	
+	        inorder(node.left);
+	        if (pre != null) {
+	            min = Math.min(min, node.val - pre.val);
+	        }
+	        pre = node;
+	        inorder(node.right);
+	    }
+	
+}
+

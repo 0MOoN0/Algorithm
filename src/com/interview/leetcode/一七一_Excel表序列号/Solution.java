76c5ecd64b8d7fe369c3f089ce0ca82fc5971be6
0MOoN0 <814429032@qq.com>
2019-09-02 21:04:06 +0800

    leetcode-一七一_Excel表序列号

---
+++ src/com/interview/leetcode/一七一_Excel表序列号/Solution.java
@@ -0,0 +1,22 @@
+package com.interview.leetcode.一七一_Excel表序列号;
+/**
+ * 根据规律，可以得出第n位的值为:26^(n-1)*数值
+ * 数值：字符值-'A'+1
+ * @author Peter
+ *
+ */
+class Solution {
+    public int titleToNumber(String s) {
+        int pow = s.length()-1;
+        int result = 0;
+        for(int i=0; i<s.length(); i++){
+            // 获取当前字符
+            char indexC = s.charAt(i);
+            // 当前值
+            int indexI = (indexC-'A'+1);
+            result += indexI * Math.pow(26,pow);
+            pow--;
+        }
+        return result;
+    }
+}

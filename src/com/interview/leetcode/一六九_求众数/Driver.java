a02a0b9c71e422266fd8ce196b1dbb47b68a3eb3
0MOoN0 <814429032@qq.com>
2019-09-21 15:31:36 +0800

    leetcode-一六九_求众数

---
+++ src/com/interview/leetcode/一六九_求众数/Driver.java
@@ -0,0 +1,10 @@
+package com.interview.leetcode.一六九_求众数;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.majorityElement(new int[] {3,2,3}));;
+	}
+
+}
--- src/com/test/two/Main.java
+++ src/com/test/two/Main.java
@@ -1,11 +1,17 @@
 package com.test.two;
 
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+		Iterator<Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+		while(iterator.hasNext()) {
+			iterator.next();
+		}
 	}
 
 }

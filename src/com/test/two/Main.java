79cc5407420126dcb51f81ba7d9e1e99901dba7a
0MOoN0 <814429032@qq.com>
2019-09-28 23:28:48 +0800

    leetcode-六九_x的平方根

--- src/com/test/two/Main.java
+++ src/com/test/two/Main.java
@@ -5,18 +5,34 @@ import java.util.Scanner;
 public class Main {
 	
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String target = in.nextLine();
-        String[] split = target.split(" ");
-        StringBuffer sb = new StringBuffer();
-        for(int i=split.length-1; i>0; i--) {
-        	if(i!=0) {
-        		sb.append(split[i]+" ");
-        	}else {
-        		sb.append(split[i]);
-        	}
-        }
-        System.out.println(sb.toString());
+    	Scanner s = new Scanner(System.in);
+    	while(s.hasNextLine()) {
+    		// 输入格式： 体重 身高
+    		String[] data = s.nextLine().split(" ");
+    		// BIM
+    		double BIM = Double.parseDouble(data[0]) / Math.pow(Double.parseDouble(data[1]), 2);
+    		// 判断国内
+    		if(BIM < 18.5) {
+    			System.out.println("国内标准指数：偏瘦");
+    		}else if(BIM < 24) {
+    			System.out.println("国内标准指数：正常");
+    		}else if(BIM < 28) {
+    			System.out.println("国内标准指数：偏胖");
+    		}else if(BIM >= 28) {
+    			System.out.println("国内标准指数：肥胖");
+    		}
+    		// 判断国际
+    		if(BIM < 18.5) {
+    			System.out.println("国际标准指数：偏瘦");
+    		}else if(BIM < 25) {
+    			System.out.println("国际标准指数：正常");
+    		}else if(BIM < 30) {
+    			System.out.println("国际标准指数：偏胖");
+    		}else if(BIM >= 30) {
+    			System.out.println("国际标准指数：肥胖");
+    		}    		
+    		
+    	}
     }
 
 }

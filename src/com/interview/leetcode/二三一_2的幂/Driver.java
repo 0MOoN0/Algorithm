4bb303280ab5e3e69583d99855033ba8d278cdaa
0MOoN0 <814429032@qq.com>
2019-09-30 23:09:44 +0800

    leetcode-二三一_2的幂

---
+++ src/com/interview/leetcode/二三一_2的幂/Driver.java
@@ -0,0 +1,8 @@
+package com.interview.leetcode.二三一_2的幂;
+
+public class Driver {
+	public static void main(String[] args) {
+		Solution2 s = new Solution2();
+		System.out.println(s.isPowerOfTwo(2));
+	}
+}

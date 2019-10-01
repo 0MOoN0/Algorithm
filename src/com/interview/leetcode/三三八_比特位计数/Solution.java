74aa1c0fe9555c9b20c3d80f053388988d84ab69
0MOoN0 <814429032@qq.com>
2019-10-01 19:30:26 +0800

    leetcode-三三八_比特位计数

---
+++ src/com/interview/leetcode/三三八_比特位计数/Solution.java
@@ -0,0 +1,17 @@
+package com.interview.leetcode.三三八_比特位计数;
+
+class Solution {
+	public int[] countBits(int num) {
+		int[] result = new int[num + 1];
+		for (int i = 0; i <= num; i++) {
+			int numCount = 0;
+			int n = i;
+			while (n != 0) {
+				n &= (n - 1);
+				numCount++;
+			}
+			result[i] = numCount;
+		}
+		return result;
+	}
+}

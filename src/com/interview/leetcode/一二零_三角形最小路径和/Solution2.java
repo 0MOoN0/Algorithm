36191ab645123f07763078c5306be61aa1e0c555
0MOoN0 <814429032@qq.com>
2019-10-10 18:39:02 +0800

    leetcode-一二零_三角形最小路径和

---
+++ src/com/interview/leetcode/一二零_三角形最小路径和/Solution2.java
@@ -0,0 +1,32 @@
+package com.interview.leetcode.一二零_三角形最小路径和;
+
+import java.util.List;
+
+/**
+ 使用双重循环-AC-6ms
+1. 每行的数字都只会使用一次
+2. 第(x, y)个数的最小路径 = Min ( (x+1, y) , (x+1, y+1) )
+综上，可以不开辟新的空间找出最小路径和，但是会对原数组修改
+ * @author Peter
+ *
+ */
+class Solution2 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        // 对一行的数字单独判断
+        if(triangle.size()==1){
+            return triangle.get(0).get(0);
+        }
+        for(int i=triangle.size()-2; i>=0; i--){
+            List<Integer> row = triangle.get(i);
+            int col = row.size();
+
+            for(int j=0; j<col; j++){
+                int colNum = row.get(j);
+                int left = colNum+triangle.get(i+1).get(j);
+                int right = colNum+triangle.get(i+1).get(j+1);
+                row.set(j, Math.min(left, right));
+            }
+        } 
+        return triangle.get(0).get(0);
+    }
+}
---
+++ src/com/interview/leetcode/一二零_三角形最小路径和/Solution3.java
@@ -0,0 +1,24 @@
+package com.interview.leetcode.一二零_三角形最小路径和;
+
+import java.util.List;
+
+/**
+动态规划(状态压缩)-AC-2ms
+1. 每行的数字都只会使用一次
+2. 每一行的数字个数=当前行数
+ * @author Peter
+ *
+ */
+class Solution3 {
+	public int minimumTotal(List<List<Integer>> triangle) {
+		int rowNum = triangle.size(); // 行数
+		int[] nums = new int[rowNum + 1];
+		for (int row = rowNum - 1; row >= 0; row--) {
+			// 计算第row行的最佳状态，如果是最后一行，则对数组进行初始化
+			for (int col = 0; col <= row; col++) {
+				nums[col] = Math.min(nums[col], nums[col + 1]) + triangle.get(row).get(col);
+			}
+		}
+		return nums[0];
+	}
+}

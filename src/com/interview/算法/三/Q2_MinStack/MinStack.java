cec44898ea060aa5eede14f2ed8d400941724927
0MOoN0 <814429032@qq.com>
2020-02-07 12:28:46 +0800

    新增返回最小值的栈实现

---
+++ src/com/interview/算法/三/Q2_MinStack/MinStack.java
@@ -0,0 +1,58 @@
+package com.interview.算法.三.Q2_MinStack;
+
+import com.interview.算法.三.Q2_stack_queue.Array_To_Stack;
+
+/**
+ * 实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返 回栈中最小元素的操作。
+ * 
+ * @author Peter
+ *
+ */
+public class MinStack {
+
+	private Array_To_Stack stack;
+	private Array_To_Stack minStack;
+
+	public static void main(String[] args) {
+		MinStack ms = new MinStack(4);
+		ms.push(4);
+		System.out.println(ms.getMin());
+		ms.push(1);
+		System.out.println(ms.getMin());
+		ms.push(3);
+		System.out.println(ms.getMin());
+		ms.push(-1);
+		System.out.println(ms.getMin());
+		
+	}
+	
+	public MinStack(int size) {
+		stack = new Array_To_Stack(size);
+		minStack = new Array_To_Stack(size);
+	}
+
+	public Integer peek() {
+		return stack.peek();
+	}
+
+	public void push(int num) {
+		stack.push(num);
+		if(minStack.peek()==null) {
+			minStack.push(num);
+		}else if(minStack.peek()>num) {
+			minStack.push(num);
+		}else {
+			minStack.push(minStack.peek());
+		}
+	}
+
+	public Integer pop() {
+		minStack.pop();
+		return stack.pop();
+	}
+	
+	public Integer getMin() {
+		return minStack.peek();
+	}
+
+}

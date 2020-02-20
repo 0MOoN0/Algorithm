c16929c64726caaa792ffdd4091dce19f5b87c56
0MOoN0 <814429032@qq.com>
2020-02-20 14:54:53 +0800

    链表荷兰国旗问题(解法2，保持稳定性，一次遍历，额外空间O(1))

--- src/com/interview/算法/三/Q12_SmallerEqualBigger/SmallerEqualBigger.java
+++ src/com/interview/算法/三/Q12_SmallerEqualBigger/SmallerEqualBigger.java
@@ -6,6 +6,8 @@ import com.interview.算法.utils.Utils;
 /**
  * 将单向链表按某值划分成左边小、中间相等、右边大的形式
  * 将链表划分为小于K，等于K，大于K三部分
+ * 方法一：使用数组+数组的荷兰国旗问题 (无法保证元素的稳定性)
+ * 方法二：类似荷兰国旗问题，使用链表完成，将链表划分为大于K，等于K，小于K，三条链表， 再将他们连接起来(可以保证元素的稳定性)
  * @author Peter
  *
  */
@@ -53,6 +55,58 @@ public class SmallerEqualBigger {
 		nodes[B] = temp;
 	}
 	
+	
+	public static Node smallerEqualBigger2(Node p, int K) {
+		// 定义三个链表域，大于，小于，等于
+		Node bigH = null;	// 大于域的头
+		Node bigT = null;	// 大于域的尾
+		Node equalH = null;
+		Node equalT = null;
+		Node smallH = null;
+		Node smallT = null;
+		Node next = null;	// 下一个节点
+		while(p != null) {
+			next = p.next;
+			p.next = null;
+			if(p.value < K) {
+				if(smallH == null) {
+					smallH = p;
+					smallT = p;
+				}else {
+					smallT.next = p;
+					smallT = smallT.next;
+				}
+				
+			}else if(p.value == K) {
+				if(equalH == null) {
+					equalH = p;
+					equalT = p;
+				}else {
+					equalT.next = p;
+					equalT = equalT.next;
+				}
+			}else {
+				if(bigH == null) {
+					bigH = p;
+					bigT = p;
+				}else {
+					bigT.next = p;
+					bigT = bigT.next;
+				}
+			}
+			p = next;
+		}
+		// 连接三条链表
+		if(smallH != null) {
+			smallT.next = equalH != null ? equalH : bigH;
+		}
+		if(equalH != null) {
+			equalT.next = bigH;
+		}
+		return smallH == null ? (equalH == null? bigH : equalH) :smallH;
+	}
+	
+	
 	public static void main(String[] args) {
 		Node head1 = new Node(7);
 		head1.next = new Node(9);
@@ -64,6 +118,15 @@ public class SmallerEqualBigger {
 		Utils.printLinkedList(head1);
 		Node result1 = smallerEqualBigger(head1, 6);
 		Utils.printLinkedList(result1);
+		Node head2 = new Node(7);
+		head2.next = new Node(9);
+		head2.next.next = new Node(1);
+		head2.next.next.next = new Node(8);
+		head2.next.next.next.next = new Node(5);
+		head2.next.next.next.next.next = new Node(2);
+		head2.next.next.next.next.next.next = new Node(5);
+		Node result2 = smallerEqualBigger2(head2, 6);
+		Utils.printLinkedList(result2);
 		
 	}
 

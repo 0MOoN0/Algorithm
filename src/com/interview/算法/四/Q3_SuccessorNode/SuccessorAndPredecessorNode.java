22ac12485b5c56e51240928fff9cbbe8e5ba209a
0MOoN0 <814429032@qq.com>
2020-02-26 18:40:48 +0800

    在二叉树中找到一个节点的后继节点和前继节点

--- src/com/interview/算法/四/Q3_SuccessorNode/SuccessorAndPredecessorNode.java
+++ src/com/interview/算法/四/Q3_SuccessorNode/SuccessorAndPredecessorNode.java
@@ -1,15 +1,23 @@
 package com.interview.算法.四.Q3_SuccessorNode;
 
 /**
+ * 在二叉树中找到一个节点的后继节点和前继节点，不用遍历整棵树
  * 在二叉树中找到一个节点的后继节点
  * 后继结点：如果一棵二叉树的中序遍历如下： 1、2、3。则2为1的后继，3为2的后继
  * 二叉树某个节点如果有右子树，则后继节点为该节点的右子树的最左边的节点（中序遍历的规律）
  * 二叉树某个节点如果没有右子树，则后继节点为以该节点作为左子树最后一个节点的节点，否则该节点不存在后继
+ * 在二叉树中找到一个节点的前继节点
+ * 与找后继节点的方法刚好相反
  * @author Peter
  *
  */
-public class SuccessorNode {
+public class SuccessorAndPredecessorNode {
 	
+	/**
+	 * 找到后继节点
+	 * @param root
+	 * @return
+	 */
 	public static Node findSuccessorNode(Node root) {
 		// 有右子树
 		if(root != null && root.right != null) {
@@ -28,6 +36,33 @@ public class SuccessorNode {
 		}
 		return null;
 	}
+	
+	/**
+	 * 找到前继节点
+	 * @param root
+	 * @return
+	 */
+	public static Node findPredecessorNode(Node root) {
+		if(root == null) {
+			return null;
+		}
+		// 判断是否有左子树
+		if(root.left != null) {		// 如果有左子树，找到左子树最右边的节点
+			root = root.left;
+			while(root.right != null) {
+				root = root.right;
+			}
+			return root;
+		}else {		// 没有左子树，找到第一个右子树指向当前节点或当前节点所在的子树的节点（如案例所示的7的前继）
+			Node pre = root.parent;
+			while(pre != null && pre.right != root) {
+				root = pre;
+				pre = pre.parent;
+			}
+			return pre;
+		}
+	}
+	
 	
 
 	public static class Node {
@@ -87,6 +122,29 @@ public class SuccessorNode {
 		System.out.println(test.value + " next: " + findSuccessorNode(test).value);
 		test = head.right.right; // 10's next is null
 		System.out.println(test.value + " next: " + findSuccessorNode(test));
+		
+		System.out.println("//===========findPredecessorNode==========");
+		Node test2 = head.left.left;	// 1's pre is null
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.left.left.right;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.left;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.left.right;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.left.right.right;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.right.left.left;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.right.left;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.right;
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));
+		test2 = head.right.right; // 10's next is null
+		System.out.println(test2.value + " next: " + findPredecessorNode(test2));		
+		
 	}
 
 }

37b6ba267bb9256be66128114c21be82a5fac877
0MOoN0 <814429032@qq.com>
2021-07-05 11:47:55 +0800

    leetcode_二零六_反转链表_题解二

--- src/com/interview/leetcode/二零六_反转链表/ListNode.java
+++ src/com/interview/leetcode/二零六_反转链表/ListNode.java
@@ -3,8 +3,7 @@ package com.interview.leetcode.二零六_反转链表;
 public class ListNode {
 	int val;
 	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-	}
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,10 +2,14 @@ package com.test;
 
 class Test {
     public static void main(String[] args) throws Exception {
-        String str1 = new String("a");
-        String str2 = new String("a");
-        System.out.println(str1.equals(str2));
-        System.out.println(str1==str2);
-        System.out.println(str1.hashCode() == str2.hashCode());
+        Integer a = new Integer(2);
+        Integer b = new Integer(3);
+        System.out.println(a);
+        Swap(a,b);
+        System.out.println(a);
+    }
+
+    public static void Swap(Integer a, Integer b){
+        System.out.println();
     }
 }

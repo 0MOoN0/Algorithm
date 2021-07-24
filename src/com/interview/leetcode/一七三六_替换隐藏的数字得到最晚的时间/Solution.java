4523a6ee8f9f29321b55c133b72aa77e62926b79
0MOoN0 <814429032@qq.com>
2021-07-25 00:16:22 +0800

    leetcode_一七三六_替换隐藏的数字得到最晚的时间_一种题解 leetcode_一八九三_检查是否区域内所有整数都被覆盖_一种题解

---
+++ src/com/interview/leetcode/一七三六_替换隐藏的数字得到最晚的时间/Solution.java
@@ -0,0 +1,25 @@
+package com.interview.leetcode.一七三六_替换隐藏的数字得到最晚的时间;
+
+/**
+ * @author Leon
+ */
+public class Solution {
+    public String maximumTime(String time) {
+        char[] chars = time.toCharArray();
+        if (chars[0] == '?' && chars[1] == '?') {
+            chars[0] = '2';
+            chars[1] = '3';
+        } else if (chars[0] == '?') {
+            chars[0] = chars[1] - '0' < 4 ? '2' : '1';
+        } else if (chars[1] == '?') {
+            chars[1] = chars[0] - '0' < 2 ? '9' : '3';
+        }
+        if(chars[3] == '?'){
+            chars[3] = '5';
+        }
+        if(chars[4] == '?'){
+            chars[4] = '9';
+        }
+        return new String(chars);
+    }
+}
--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -3,16 +3,40 @@ package com.test;
 import com.interview.leetcode.一零零_相同的树.TreeNode;
 import com.interview.leetcode.五八_最后一个单词的长度.Solution2;
 
+import java.lang.reflect.Constructor;
 import java.sql.ResultSet;
 import java.util.*;
 import java.util.stream.Stream;
 
 class Test {
-    public static void main(String[] args){
-        Queue<Integer> queue = new LinkedList<Integer>();
-        Integer i = new Integer(1);
-        queue.add(i);
-        queue.add(i);
-        queue.stream().forEach(System.out::println);
+    public static void main(String[] args) throws Exception {
+        Class<Singleton> clazz = Singleton.class;
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        for(Constructor constructor : declaredConstructors){
+            constructor.setAccessible(true);
+            Object o1 = constructor.newInstance();
+            Object o2 = constructor.newInstance();
+            System.out.println(o1==o2);
+            System.out.println(o1);
+            System.out.println(o2);
+        }
+    }
+}
+
+class Singleton {
+    private volatile static Singleton uniqueSingleton;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (null == uniqueSingleton) {
+            synchronized (Singleton.class) {
+                if (null == uniqueSingleton) {
+                    uniqueSingleton = new Singleton();
+                }
+            }
+        }
+        return uniqueSingleton;
     }
 }

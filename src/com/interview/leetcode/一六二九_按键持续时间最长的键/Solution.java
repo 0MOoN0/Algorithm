925d277dbd1e1c4e51e8d8668089d5e37efd09d4
0MOoN0 <814429032@qq.com>
2022-01-09 23:24:43 +0800

    [UPDATE]新增leetcode_一六二九_按键持续时间最长的键题解

---
+++ src/com/interview/leetcode/一六二九_按键持续时间最长的键/Solution.java
@@ -0,0 +1,22 @@
+package com.interview.leetcode.一六二九_按键持续时间最长的键;
+
+/**
+ * HashMap
+ *
+ * @author Leon
+ */
+public class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        int maxTimes = releaseTimes[0];
+        char result = keysPressed.charAt(0);
+        // 遍历数组
+        for (int i = 1; i < releaseTimes.length; i++) {
+            int diff_time = releaseTimes[i] - releaseTimes[i - 1];
+            if (diff_time > maxTimes || (diff_time == maxTimes && keysPressed.charAt(i) > result)) {
+                maxTimes = diff_time;
+                result = keysPressed.charAt(i);
+            }
+        }
+        return result;
+    }
+}
